86b800c519ceb634c7f3e4c611be348146c2d56b
jacqueline-homan <jacquelinehoman7@gmail.com>
2014-09-20 23:56:44 -0400

    Added index file in album/views template, added CRUD actions on albums controller except for delete and edit

---
+++ app/controllers/albums_controller.rb
@@ -0,0 +1,31 @@
+class AlbumsController < ApplicationController
+  def index
+  	@albums = Album.all
+  end
+
+  def new
+  	@album = Album.new
+  end
+
+  def create
+    @album = Album.new(album_params)
+    @album.save
+    redirect_to albums_path
+  end
+
+  def show
+    @album = Album.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def album_params
+  	params.require(:name).permit(:id, :category)
+  end
+end
